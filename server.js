134d4f6ebd303619fc0efacdad59200ab60b90f5
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-03 11:33:52 -0400

    Home Route Created

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+//CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} 😎`);
+  });
