956e1f6da133f61e7d785d4f26d499be4ebb7abc
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-08 16:35:36 -0400

    Added a validation for image url

--- validations/validations.js
+++ validations/validations.js
@@ -37,9 +37,23 @@ const checkRating = (req, res, next) => {
     }
 }
 
+const validateURL = (req, res, next) => {
+    if (
+      req.body.candy_image.substring(0, 7) === "http://" ||
+      req.body.candy_image.substring(0, 8) === "https://"
+    ) {
+      return next();
+    } else {
+      res
+        .status(400)
+        .json({ error: "You forgot to start your image link with http:// or https://" });
+    }
+  };
+
 module.exports = {
     checkName,
     checkIsBoolean,
     checkPrice,
-    checkRating
+    checkRating,
+    validateURL
 }
